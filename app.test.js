ad03ac78bfed51f2f1e84ea5007fdf8430d571bb
gonzu <gonzalodiezdavila@gmail.com>
2023-01-25 23:06:36 +0100

    push function n tests

--- app.test.js
+++ app.test.js
@@ -18,10 +18,22 @@ describe('Given array functions', () => {
   });
   describe('When using toPush function u could add items to an array.', () => {
     test('Then if you have an array of numbers and you want to add a string, you should get it done', () => {
-      const array = [1, 2, 3, 4, 5];
-      const arrayxd = [1, 2, 3, 4, 5, 6];
-      const r = toPush(array, 6);
-      expect(r).toBe(arrayxd);
+      const a = [1, 2, 3];
+      const b = 4;
+      const expected = 4;
+      const result = toPush(a, b);
+      expect(result).toEqual(expected);
+      expect(a).toEqual([1, 2, 3, 4]);
+    });
+  });
+  describe('When using toPush function u could add items to an array.', () => {
+    test('Then pushes an element to an empty array and returns the new length', () => {
+      const a = [];
+      const b = 1;
+      const expected = 1;
+      const result = toPush(a, b);
+      expect(result).toEqual(expected);
+      expect(a).toEqual([1]);
     });
   });
 });
--- length.js
+++ length.js
@@ -1,7 +1,7 @@
 export const aLength = (arr) => {
   let lengthValue = 0;
   for (const i of arr) {
-    lengthValue = i;
+    lengthValue++;
   }
 
   return lengthValue;
