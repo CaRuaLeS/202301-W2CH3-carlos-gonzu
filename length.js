00989e0eceb5cc56c0574cff35d4ac0df8bd079a
gonzu <gonzalodiezdavila@gmail.com>
2023-01-25 19:02:58 +0100

    add length.js file

---
+++ length.js
@@ -0,0 +1,12 @@
+const array = [];
+
+export const aLength = (arr) => {
+  let lengthValue = 0;
+  for (const i of arr) {
+    lengthValue = i;
+  }
+
+  return lengthValue;
+};
+
+console.log(aLength(array));
